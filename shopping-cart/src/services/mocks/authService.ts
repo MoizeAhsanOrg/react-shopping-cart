4485f68eb55b480255fdf7bc1871bbfd83b418ec
Moize Ahsan <moize.ahsan@10pearls.com>
2024-10-07 18:35:12 +0500

    refactor: mock users
    - refactor mock uses to mock data directory

--- shopping-cart/src/services/mocks/authService.ts
+++ shopping-cart/src/services/mocks/authService.ts
@@ -1,9 +1,5 @@
-import { User } from '../../types/User';
-
-const users: User[] = [
-    { username: 'admin', password: 'admin123', role: 'admin', name: 'Admin', email: 'admin@admin.com', history: [] },
-    { username: 'customer', password: 'customer123', role: 'customer', name: 'Customer', email: 'customer@customer.com', history: [] },
-];
+import { User } from "../../types/User";
+import { users } from "./data/users";
 
 export const authenticateUser = (username: string, password: string): User | null => {
     console.log("here")
