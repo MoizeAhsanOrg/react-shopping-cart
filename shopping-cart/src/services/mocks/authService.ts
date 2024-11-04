71a751a0bf83c568c10a66d68696c49a678283f0
Moize Ahsan <moize.ahsan@10pearls.com>
2024-11-04 14:19:49 +0500

    Create better sign in experience (#11)
    * feat: add loader on login screen
    
    * feat: add encrytion/decryption to hold user data
    
    * refactor: fix lints
    
    - remove visual test addon
    - fix lint issues

--- shopping-cart/src/services/mocks/authService.ts
+++ shopping-cart/src/services/mocks/authService.ts
@@ -1,7 +1,10 @@
 import { User } from '../../types/User';
 import { users } from './data/users';
 
-export const authenticateUser = (username: string, password: string): User | null => {
+export const authenticateUser = async (
+  username: string,
+  password: string): Promise<User | null> => {
   const user = users.find(user => user.username === username && user.password === password) || null;
+  await new Promise(resolve => setTimeout(resolve, 3000));
   return user;
 };
